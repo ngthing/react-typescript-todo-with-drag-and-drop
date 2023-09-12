653ae74fb7bd13973c3e6d24afeb791688d09e4a
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-09-12 12:00:26 -0500

    Update test - todo add valid tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -7,9 +6,8 @@ describe('App', () => {
     it('renders App component and has a default Todo list', () => {
         render(<App />);
         expect(screen.getByLabelText('todoName')).toBeInTheDocument();
-        expect(screen.getByLabelText('todo-list')).toBeInTheDocument();
         expect(screen.getByLabelText('add-todo')).toBeInTheDocument();
         expect(screen.getByLabelText('delete-todo-list')).toBeInTheDocument();
-        expect(screen.getAllByLabelText('delete-todo').length).toBe(4);
     });
 });
+
