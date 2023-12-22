be78f862e24a3830e5a4a90d36267e3cdb06cbab
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-12-22 11:30:03 -0500

    use styled components library and clean up css

---
+++ src/StyledComponents.tsx
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+
+const background = '#f9f2f2';
+const textColor = 'rgb(43, 39, 39)';
+const checkboxBorder = '#3d9863';
+const checkboxColor = '#6ec26eb5';
+const white = '#ffffff';
+const todoBorder = '#c0bbbb8f';
+const btnTextColor = '#2f2b2b';
+const actionBtnHoveredColor = '#ece2e2';
+const actionBtnColor = '#c6b9b9';
+
+export const FlexContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+export const ListTitle = styled.div`
+    padding-top: 10vh;
+    padding-bottom: 5vh;
+`;
+export const TitleInput = styled.input`
+    font-size: 3rem;
+    text-align: center;
+    cursor: pointer;
+    border: none;
+    background: transparent;
+    outline: none;
+    width: 90%;
+    font-family: inherit;
+    height: fit-content;
+`;
+
+export const ListContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 20px;
+`;
+
+export const ItemContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 0.25rem 0;
+    border: solid ${todoBorder};
+    padding: 2px 10px;
+    border-radius: 10px;
+    min-width: 50%;
+    width: 80%;
+    position: relative;
+`;
+
+export const ItemIndex = styled.span`
+    cursor: pointer;
+`
+
+export const ItemCheckbox = styled.div`
+    margin: 0 10px;
+    display: flex;
+    width: 25px;
+    height: 22px;
+    border-radius: 50%;
+    font-size: 13px;
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
+    transition: background-color .2s ease-in-out;
+    border: 1px solid var(checkboxBorder);
+    &:hover{
+        background: var(checkboxColor);
+        border: 1px solid rgba(255, 255, 255, 0);
+    }
+`
+
+export const ItemTextContainer = styled.div`
+    width: 100%;
+    padding-right: 3%;
+    display: inline-block;
+`
+
+export const ItemText = styled.textarea`
+    border: none;
+    background: transparent;
+    font-size: 1.2rem;
+    outline: none;
+    width: 100%;
+    font-family: inherit;
+    /* text-wrap: pretty; */
+    overflow: auto;
+    resize: none;
+    height: fit-content;
+    /* necessary for line break in textarea works in mobile browser */
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+    white-space: pre-line;
+
+    &::-webkit-scrollbar {
+        width: 4px;
+    }
+    &::-webkit-scrollbar-track {background: ${background};}
+    &::-webkit-scrollbar-thumb {
+        background: ${todoBorder};
+    }
+`
+
+export const Button = styled.button`
+    border-color: transparent;
+    color:  ${btnTextColor};
+    cursor: pointer;
+    &:hover{
+        background:${actionBtnHoveredColor}
+    }
+`
--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,5 +1,5 @@
-import { Ref } from 'react';
 import { Draggable } from "react-beautiful-dnd";
+import { Button, ItemContainer, ItemCheckbox, ItemIndex, ItemTextContainer, ItemText } from './StyledComponents';
 
 export interface Todo {
     id: string; content: string; isCompleted: boolean
@@ -11,23 +11,8 @@ interface TodoItemProps extends Todo {
     handleDelete: (i: number) => void;
     handleKeyDown: (e: React.KeyboardEvent<HTMLElement>, i: number) => void;
     toggleTodoCompleteAtIndex: (i: number) => void;
-    mRef?: Ref<HTMLDivElement> | null;
 };
 
-export const TodoItem = (ps: TodoItemProps) => (
-    <div className={`todo ${ps.isCompleted && 'todo-is-completed'}`} ref={ps.mRef}>
-        <span className='todo-index'>#{ps.index + 1} </span>
-        <div className="checkbox" onClick={() => ps.toggleTodoCompleteAtIndex(ps.index)}>
-            {ps.isCompleted && (
-                <span>&#x2714;</span>
-            )}
-        </div>
-        <input type="text" value={ps.content} id={`todo-input-${ps.index}`} aria-label={`todo-input-${ps.index}`}
-            onKeyDown={(e) => ps.handleKeyDown(e, ps.index)}
-            onChange={(e) => ps.onChange(e.target.value, ps.index)} />
-        <button className="btn-action" aria-label='delete-todo' onClick={() => ps.handleDelete(ps.index)}>&#x2715;</button>
-    </div>
-)
 const todoTextareaHeight = (content: string): number => {
     // it works! muahaha - why these formula and numbers - no comment. 
     // but 25px is the min width i want to set.
@@ -39,25 +24,25 @@ const todoTextareaHeight = (content: string): number => {
 export const DragableTodoItem = (ps: TodoItemProps) => (
     <Draggable key={ps.id} draggableId={ps.id} index={ps.index}>
         {(provided) => (
-            <div key={ps.id} className={`todo ${ps.isCompleted && 'todo-is-completed'}`}
+            <ItemContainer key={ps.id} className={`${ps.isCompleted && 'todo-is-completed'}`}
                 ref={provided.innerRef}
                 {...provided.draggableProps}
                 {...provided.dragHandleProps}
             >
-                <span className='todo-index'>#{ps.index + 1} </span>
-                <div className="checkbox" onClick={() => ps.toggleTodoCompleteAtIndex(ps.index)}>
+                <ItemIndex className='todo-index'>#{ps.index + 1} </ItemIndex>
+                <ItemCheckbox className='checkbox' onClick={() => ps.toggleTodoCompleteAtIndex(ps.index)}>
                     {ps.isCompleted && (
                         <span>&#x2714;</span>
                     )}
-                </div>
-                <div className="todo-text">
-                    <textarea className='todoTextarea' wrap="off" value={ps.content} id={`todo-input-${ps.index}`} aria-label={`todo-input-${ps.index}`}
+                </ItemCheckbox>
+                <ItemTextContainer>
+                    <ItemText className="todoTextarea" wrap="off" value={ps.content} id={`todo-input-${ps.index}`} aria-label={`todo-input-${ps.index}`}
                         style={{ height: todoTextareaHeight(ps.content) }}
                         onKeyDown={(e) => ps.handleKeyDown(e, ps.index)}
                         onChange={(e) => ps.onChange(e.target.value, ps.index)} />
-                </div>
-                <button className="btn-action btn-delete-todo" aria-label='delete-todo' onClick={() => ps.handleDelete(ps.index)}>&#x2715;</button>
-            </div>
+                </ItemTextContainer>
+                <Button className="btn-delete-todo" aria-label='delete-todo' onClick={() => ps.handleDelete(ps.index)}>&#x2715;</Button>
+            </ItemContainer>
         )}
     </Draggable>
 ) 
