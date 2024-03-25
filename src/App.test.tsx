2b166ebf6db51c5e6818de2bf073a2314990212f
Thi Nguyen <ngthi.nguyen21@gmail.com>
2024-03-25 18:35:48 -0400

    Update App dom test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable testing-library/no-debugging-utils */
-import { render, screen } from "@testing-library/react";
 import App from "./App";
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
 describe("App", () => {
-	it("renders App component and has a default Todo list", () => {
+	it("renders App component and has a Todo list name and default action buttons", () => {
 		render(<App />);
 		expect(screen.getByLabelText("todoName")).toBeInTheDocument();
 		expect(screen.getByLabelText("add-todo")).toBeInTheDocument();
 		expect(screen.getByLabelText("delete-todo-list")).toBeInTheDocument();
 		expect(screen.getByLabelText("check-all-todo")).toBeInTheDocument();
 		expect(screen.getByLabelText("uncheck-all-todo")).toBeInTheDocument();
+		expect(screen.getByLabelText("copy-list")).toBeInTheDocument();
 	});
 });
